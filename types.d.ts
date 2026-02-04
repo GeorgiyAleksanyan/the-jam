35f747db4de33a2ef26bd473b4140f952b7e5772
Sovereign <sovereign@webglo.org>
2026-02-04 18:35:06 +0000

    fix: Add missing type definitions for PrismJS and CodeEditor

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,5 @@
 declare module 'vm2';
+declare module 'prismjs/components/prism-core';
+declare module 'prismjs/components/prism-clike';
+declare module 'prismjs/components/prism-javascript';
+declare module 'react-simple-code-editor';
